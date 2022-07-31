f035a7c2511563acc497668d7355745077d7b9a7
Hamza Ben Abdallah <hamza.ben.abdallah02@gmail.com>
2022-07-31 14:50:02 -0400

    created routes and globalcontext

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { GlobalProvider } from "./services/GlobalContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
