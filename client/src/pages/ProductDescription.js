a95a745cc12d930c304bd5e0a196545a0958d424
“rashid <rashidsabag@gmail.com>
2022-08-03 22:16:51 -0400

    updatng the product page styles

--- client/src/pages/ProductDescription.js
+++ client/src/pages/ProductDescription.js
@@ -2,8 +2,13 @@ import styled from "styled-components";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { GlobalContext } from "../services/GlobalContext";
 
 const ProductDescription = () => {
+  // item id's in cart
+  // const { itemsIdsInCart, setItemsIdsInCart } = useContext(GlobalContext);
+
   const [productInformation, setProductInformation] = useState();
   let { product_id } = useParams();
   product_id = parseInt(product_id);
@@ -14,6 +19,7 @@ const ProductDescription = () => {
       .then((data) => setProductInformation(data.data[0]));
   }, []);
   console.log("productInformation", productInformation);
+  console.log("itemsIdsInCart", itemsIdsInCart);
   return (
     <>
       {productInformation ? (
@@ -28,11 +34,10 @@ const ProductDescription = () => {
             <div className="productInfoDiv">
               <span>{productInformation.category}</span>
               <h2>{productInformation.name}</h2>
-              <span>{`Product #: ${productInformation._id}`}</span>
+              <span className="productId">{`Product #: ${productInformation._id}`}</span>
+              <h3 className="price">{productInformation.price}</h3>
             </div>
-            <div className="pricing_stock">
-              <h3>{productInformation.price}</h3>
-            </div>
+
             <button>Add to cart</button>
           </div>
         </Wrapper>
@@ -56,30 +61,51 @@ const Wrapper = styled.div`
   align-items: center;
   border: solid 1px black;
   button {
-    text-align: left;
-    width: 30%;
-    margin-top: 30px;
+  width: 100%;
+  background-color: red;
+  border: none;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  font-weight: bold;
+  border-radius: 5px;
+  cursor: pointer;
+}
   }
 
   .imageWrapper {
-    border: 1px solid black;
+
     margin: 50px;
     width: 400px;
-    height: 400px;
+    height: 320px;
     display: flex;
     align-items: center;
     justify-content: center;
+    border-radius: 5px;
     img {
       width: 80%;
+      border-radius: 5px;
     }
   }
   .infoWrapper {
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     width: 400px;
-    height: 400px;
+    height: 320px;
+    border-radius: 5px;
+    .productId {
+      margin: 0;
+    }
+    .price {
+      margin-top: 20px;
+      font-size: 1.5em;
+    }
     h2 {
-      margin: 5px 0 20px 0;
+      margin: 5px 0 5px 0;
+      font-size: 1.5em;
     }
     h3 {
       padding: 0;
@@ -93,9 +119,10 @@ const Wrapper = styled.div`
   .productInfoDiv {
     border: 1px solid black;
     margin: 5px;
+    border-radius: 5px;
+    padding: 5px;
   }
 
   .pricing_stock {
-    margin-top: 40px;
   }
 `;
--- client/src/services/GlobalContext.js
+++ client/src/services/GlobalContext.js
@@ -1,7 +1,12 @@
 import { createContext } from "react";
+import { useState } from "react";
 
 export const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider value={{ itemsIdsInCart, setItemsIdsInCart }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 };
