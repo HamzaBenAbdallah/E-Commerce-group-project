21657514012cc6482ee51d066afa8f2a7a0e4689
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-24 13:35:00 -0400

    Cleanup the server

--- server/index.js
+++ server/index.js
@@ -28,18 +28,13 @@ app.use(morgan("tiny"));
 app.use(express.static("./server/assets"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use("/", express.static(__dirname + "/"));
 
 // REST endpoints?
 app.get("/get-items", getItems);
 app.get("/products/:product_id", getProductDescription);
 app.post("/checkout", createNewOrder);
 app.get("/confirmed-purchased", getOrder);
-//cart endpoints//
-// app.post("/purchase", addPurchase);
-// app.put("/items/:itemId", updateQuantity);
-// app.delete("/items/:itemId", deleteQuantity);
 
-// app.get("/bacon", (req, res) => res.status(200).json("🥓"));
+app.get("/bacon", (req, res) => res.status(200).json("🥓"));
 
 app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
